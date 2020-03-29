38413d394c9cd4bae62adcaf0e76ec81f2632759
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-03-29 18:57:16 -0300

    drawer final

--- meuapp/src/Drawer.jsx
+++ meuapp/src/Drawer.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import clsx from 'clsx';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
@@ -8,45 +7,38 @@ import List from '@material-ui/core/List';
 const useStyles = makeStyles({
   list: {
     width: 200,
-    marginTop: 30,
+    top: 50,
   },
   fullList: {
     width: 'auto',
   },
-  drawer: {
-    opacity: 0,
+  teste: {
+    top: 50,
   }
 });
 
 export default function TemporaryDrawer() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    right: false,
-  });
+  const [right, setRight] = useState(false);
 
-  const toggleDrawer = (anchor, open) => event => {
-    setState({ ...state, [anchor]: open });
-  };
 
-  const list = anchor => (
+  const list = () => (
     <div
       className={classes.list}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      onClick={() => setRight(false)}
+      onKeyDown={() => setRight(false)}
     >
-      <List>
 
-      </List>
     </div>
   );
 
   return (
     <div>
 
-      <Button onClick={toggleDrawer('right', true)}>Right</Button>
-      <Drawer anchor='right' open={state['right']} onClose={toggleDrawer('right', false)}>
-        {list('right')}
+      <Button onClick={() => setRight(true)}>Right</Button>
+      <Drawer anchor='right' BackdropProps={{ invisible: true }} classes={{ paper: classes.teste }} open={right} onClose={() => setRight(false)}>
+        {list()}
       </Drawer>
 
     </div>
